3abfd37b8d4d8729f42892a7d83e07cea1352b31
ataraciuk <ataraciuk@gmail.com>
2013-12-04 17:38:16 -0500

    first version

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$( window ).load(function() {
+	var btn = $('button');
+	var foodRow = $('table').find('tr').last();
+	var modRow = $('table').find('tr').first();
+	var currentStep = 0;
+
+	var steps = [function() {
+		modRow.hide();
+		btn.html('Draw modifiers');
+		foodRow.children().each(function(i,e){
+			$(e).html(food[Math.floor(Math.random()*food.length)]);
+		});
+	},
+	function() {
+		modRow.show();
+		btn.html('Redraw');
+		modRow.children().each(function(i,e){
+			$(e).html(modifiers[Math.floor(Math.random()*modifiers.length)]);
+		});
+	}];
+	btn.click(function(e){
+		e.preventDefault();
+		currentStep = (currentStep + 1) % steps.length;
+		steps[currentStep]();
+	});
+	steps[0]();
+
+	$(document).keyup(function(e){
+		if(e.which === 82) btn.click();
+	});
+});
